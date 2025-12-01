1375428d27a0943814ba18b6cf9de36318ca40f8
garimazala15 <garima1582000@gmail.com>
2025-12-01 14:00:33 +0530

    Add files via upload

--- DOM.js
+++ DOM.js
@@ -23,15 +23,100 @@ email.addEventListener("blur",(e)=>{
 })
 
 
+//password
+password.addEventListener("blur",()=>{
+    let password=document.getElementById("passwordError")
+    if(!password.value){
+        passwordError.innerText="please enter proper password"
+    }
+    else{
+        passwordError.innerText=""
+    }
+})
+
+//name
+first_name.addEventListener("blur",()=>{
+    let nameError=document.getElementById("nameError")
+    let name=nameError.value
+
+    if(name.length<=3){
+        nameError.innerText="please enter proper name of more than 3 chars"
+    }
+    else{
+        nameError.innerText=""
+    }
+})
+
+//last_name
+last_name.addEventListener("blur",()=>{
+    let nameError=document.getElementById("nameError")
+    let name=nameError.value
+
+    if(name.length<=3){
+        nameError.innerText="please enter proper name of more than 3 chars"
+    }
+    else{
+        nameError.innerText=""
+    }
+})
+
+//phone
+first_name.addEventListener("blur",()=>{
+    let nameError=document.getElementById("nameError")
+    let name=nameError.value
+
+    if(name.length<=3){
+        nameError.innerText="please enter proper name of more than 3 chars"
+    }
+    else{
+        passwordError.innerText=""
+    }
+})
+//company
+first_name.addEventListener("blur",()=>{
+    let nameError=document.getElementById("nameError")
+    let name=nameError.value
+
+    if(name.length<=3){
+        nameError.innerText="please enter proper name of more than 3 chars"
+    }
+    else{
+        passwordError.innerText=""
+    }
+})
+
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
 
 
-    console.log(email.value,password.value,first_name.value,last_name.value,phone.value,compamy.value)
-
-
-
-
-
-
+    console.log(email.value,password.value,first_name.value,last_name.value,phone.value,company.value)
+    
 })
+
+
+let tr=document.createElement("tr")
+
+let td1=document.createElement("td1")
+let td2=document.createElement("td2")
+let td3=document.createElement("td3")
+let td4=document.createElement("td4")
+let td5=document.createElement("td5")
+let td6=document.createElement("td6")
+
+
+td1.innerText=email.value
+td2.innerText=password.value
+td3.innerText=first_name.value
+td4.innerText=last_name.value
+td5.innerText=phone.value
+td6.innerText=company.value
+
+
+tr.appendChild(td1)
+tr.appendChild(td2)
+tr.appendChild(td3)
+tr.appendChild(td4)
+tr.appendChild(td5)
+tr.appendChild(td6)
+
+t_body.appendChild(tr)
