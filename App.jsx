c50d139f86f7f456554c8421880908fdffef6a71
garimazala15 <garima1582000@gmail.com>
2025-12-05 13:42:04 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from  'react'
+
+const App=()=>{
+  let age:25
+    return (
+      <div>
+        <h2>Age:{age}</h2>
+        <h1>Welcome to react</h1>
+      </div>
+)
+}
+
+export default App
---
+++ notes.js
@@ -0,0 +1,34 @@
+// change directory++ cd dir_name
+
+// change directory-- cd..
+
+//project create =npm create vite@latest
+//project name
+//framework name=react
+//varient name=java script
+//enter enter
+
+//get url=npm run dev
+
+//open src folder
+//remove content of app.css,index.css,app.jsx
+
+//import React from  'react'
+
+/*const app=()=>{
+    return {
+        <div>App</div>
+    }
+}
+
+export default App*/
+//naming convections
+
+//file=components
+//component create=Home.jsx(javascript+xml)
+
+//code writing method
+
+//before return=js code
+//after return=html code
+//inside html>js code use {}
