1cd9cc1d8a3950f82a41b88c7f203d9c8b7a282b
garimazala15 <garima1582000@gmail.com>
2025-12-09 14:07:19 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,13 +1,36 @@
-import React from  'react'
+import React from 'react';
 
-const App=()=>{
-  let age:25
-    return (
-      <div>
-        <h2>Age:{age}</h2>
-        <h1>Welcome to react</h1>
-      </div>
-)
-}
+const App = () => {
+  let name = "Garima";
+  let age = 25;
+  let mobile="9098673927";
+  let rating=5;
 
-export default App
+  let data=true;
+
+  if(data ===true){
+    return (<h1>loading......</h1>)
+  }
+
+  let design={
+    color:"red",
+    backgroundColor:"green",
+    fontSize:"56px",
+    fontWeight:"900"
+  }
+
+  return (
+    <div>
+      <h1 style={{color:"red"}}>Name: {name}</h1>
+      <h2 style={{backgroundcolor:"green"}}>Age: {age}</h2>
+      <h2 style={design}>Mobile:{mobile}</h2>
+      <h3>Hey welcome you all </h3>
+
+      <h1 style={{color:(age>3)?"red":"green"}}>age:{age}</h1>
+<h1 style={{color:(rating>=7)?"red":"green"}}>rating:{rating}</h1>
+
+    </div>
+  );
+};
+
+export default App;
